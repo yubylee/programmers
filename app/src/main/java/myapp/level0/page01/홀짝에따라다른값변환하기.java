ec0aac2f307ccae4eb6c15f1edc57e1c0c83e570
yubylee <yuby1003@naver.com>
2023-11-14 13:16:26 +0900

    2일차

--- app/src/main/java/myapp/level0/page01/홀짝에따라다른값변환하기.java
+++ app/src/main/java/myapp/level0/page01/홀짝에따라다른값변환하기.java
@@ -4,13 +4,13 @@ public class 홀짝에따라다른값변환하기 {
 	public int solution(int n) {
 		int answer = 0;
 		if(n % 2 != 0) {
-			for(int i = 0; i < n; i++) {
+			for(int i = 1; i <= n; i++) {
 				if(i % 2 != 0) {
 					answer += i;
 				}
 			}
 		} else {
-			for(int i = 0; i < n; i++) {
+			for(int i = 1; i <= n; i++) {
 				if(i % 2 == 0) {
 					answer += (i*i);
 				}
