e12ee4980f4ecf35557f3f19ce19a1e5c088a266
yubylee <yuby1003@naver.com>
2023-11-27 14:20:04 +0900

    pcce 기출문제

---
+++ app/src/main/java/myapp/pcce/가채점.java
@@ -0,0 +1,19 @@
+package myapp.pcce;
+
+public class 가채점 {
+	public String[] solution(int[] numbers, int[] our_score, int[] score_list) {
+        int num_student = numbers.length;
+        String[] answer = new String[num_student];
+
+        for (int i = 0; i < num_student; i++) {
+            if (our_score[i] == score_list[numbers[i]-1]) {
+                answer[i] = "Same";
+            }
+            else {
+                answer[i] = "Different";
+            }
+        }
+
+        return answer;
+    }
+}
